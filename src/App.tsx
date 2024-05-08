993aa950e69f67eafee4e630208db851eb03d298
iroshanbrian <brianstudiolk@gmail.com>
2024-05-09 00:58:21 +0530

    #testing Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-center bg-red-600 font-mono">Hello World</h1>
+      <h1 className="text-center bg-red-600 font-mono">hello World</h1>
     </div>
   )
 }
