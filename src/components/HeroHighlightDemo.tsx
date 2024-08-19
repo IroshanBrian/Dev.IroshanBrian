c1b7c9295ec282242e1594061c20acdd09fcfc9a
Iroshan Brian Bandaragoda <brianstudiolk@gmail.com>
2024-08-20 01:25:22 +0530

    chore: Update hero image to use PNG format

--- src/components/HeroHighlightDemo.tsx
+++ src/components/HeroHighlightDemo.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "../components/ui/hero-highlight";
-import me from '../assets/me.webp';
+import me from '../assets/me.png';
+import mebg from '../assets/mebg.png';
 
 export function HeroHighlightDemo() {
 
@@ -21,18 +22,20 @@ export function HeroHighlightDemo() {
                          duration: 0.5,
                          ease: [0.4, 0.0, 0.2, 1],
                     }}
-                    className="text-4xl px-4 font-title font-black md:text-4xl lg:text-5xl text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto"
+                    className="text-4xl px-4 font-title font-black md:text-4xl lg:text-5xl text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto"
                >
                     Hey, I'm Brian<span className="text-cyan-500 text-5xl">.</span><br />{" "}
-                    <Highlight className="text-3xl font-semibold">
+                    <Highlight className="text-3xl font-semibold text-black">
                          Full-Stack Developer
                     </Highlight>
                </motion.h1>
                <motion.div>
-                    <div className="bg-white lg:w-[400px] sm:w-[350px] h-[200px] rounded-full flex justify-center overflow-hidden">
-                         <img src={me} alt="Iroshan Brian" width={200} className="absolute lg:bottom-[0rem] md:bottom-[0rem] bottom-[0rem]" />
+                    <div className="bg-white mt-14 lg:w-[400px] sm:w-[350px] h-[200px] rounded-full flex justify-center items-end overflow-hidden">
+                         <img src={mebg} alt="Background" className="w-full h-auto -mt-[50px] z-10" />
+                         <img src={me} alt="Iroshan Brian" className="w-[400px] h-auto object-cover -mt-[70px] z-20 absolute" />
                     </div>
                </motion.div>
+
                {/* <motion.div>
                     <div className="bg-white lg:w-[200px] sm:w-[350px] h-[40px] rounded-full overflow-hidden text-center">
                          <h3>Time: {timeData ? timeData.time : "Loading..."}</h3>
--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -29,14 +29,14 @@ export const HeroHighlight = ({
      return (
           <div
                className={cn(
-                    "z-[-10] min-h-[100vh] flex items-center bg-white dark:bg-black justify-center w-full group",
+                    "z-[-10] min-h-[100vh] flex items-center bg-black justify-center w-full group",
                     containerClassName
                )}
                onMouseMove={handleMouseMove}
           >
-               <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 pointer-events-none" />
+               <div className="absolute inset-0 bg-dot-thick-neutral-900 pointer-events-none" />
                <motion.div
-                    className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500 absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
+                    className="pointer-events-none bg-dot-thick-lime-500 absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
                     style={{
                          WebkitMaskImage: useMotionTemplate`
             radial-gradient(
@@ -86,7 +86,7 @@ export const Highlight = ({
                     display: "inline",
                }}
                className={cn(
-                    `relative inline-block pb-1   px-1 rounded-lg bg-gradient-to-r from-indigo-300 to-purple-300 dark:from-indigo-500 dark:to-purple-500`,
+                    `relative inline-block pb-1   px-1 rounded-lg bg-gradient-to-r from-yellow-300 to-lime-300`,
                     className
                )}
           >
